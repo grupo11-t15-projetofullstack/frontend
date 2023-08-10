4483cc27a5e7aecd7d93ceb4b18a25c03810e657
felipe <felipe@hcdesenvolvimento.com.br>
2023-08-10 16:48:27 -0300

    feat footer and try responsive for mobile

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,7 @@
-import { NextPage } from "next"
-import Image from "next/image"
-import Logo from "../../assets/Logo.svg"
-import Link from "next/link"
+import { NextPage } from 'next';
+import Image from 'next/image';
+import Logo from '../../assets/Logo.svg';
+import Link from 'next/link';
 
 const DefaultHeader: NextPage = () => {
   return (
@@ -9,18 +9,18 @@ const DefaultHeader: NextPage = () => {
       <Image src={Logo} alt="Logo image" className="mx-20 my-5" />
 
       <div className="flex flex-row justify-between gap-10 border-l-2 border-grey-grey6 pl-14 h-20">
-        <Link href={"/login"} className="pt-7 text-grey-grey2 text-center">
+        <Link href="/login" className="pt-7 text-grey-grey2 text-center">
           Fazer Login
         </Link>
         <Link
-          href={"/register"}
-          className="my-auto p-1 border px-6 rounded border-grey-grey4  text-grey-grey0 font-semibold"
+          href="/register"
+          className="my-auto p-1 border px-6 rounded border-grey-grey4 text-grey-grey0 font-semibold"
         >
           Cadastrar
         </Link>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default DefaultHeader
+export default DefaultHeader;
