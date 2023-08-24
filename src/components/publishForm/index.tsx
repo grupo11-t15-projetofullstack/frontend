aefdbfb72a95be9b3560b3ace05da55f96738e46
Zevitorb <benvenuttijosevitor@gmail.com>
2023-08-24 14:20:00 -0300

    fix: inputs default value

--- src/components/publishForm/index.tsx
+++ src/components/publishForm/index.tsx
@@ -1,6 +1,6 @@
 import { usePublish } from "@/contexts/publications"
 import { ImageForm } from "../ImageForm"
-import { useState, useEffect } from "react"
+import { useState, useEffect, ChangeEvent } from "react"
 
 interface FormProps {
   toggleModal: () => void
@@ -11,26 +11,76 @@ interface Model {
   name: string
 }
 
+export interface CarInfo {
+  year: string
+  fipe: number
+  fuel: number
+}
+
+interface Car extends CarInfo {
+  model: string
+  make: string
+  color: string
+  distance: number
+  price: number
+  description: string
+  coverImg: string
+  images: string[]
+}
+
+interface CarsInfo {
+  id: string
+  name: string
+  brand: string
+  year: string
+  fuel: number
+  value: number
+}
+
 export const PublishForm = ({ toggleModal, repo }: FormProps) => {
+  const { createPublish, publishInfo, setPublishInfo } = usePublish()
   const [makerList, setMakerList] = useState<string[]>([])
   const [models, setModels] = useState<Model[]>([])
+  const [carsInfo, setCarsInfo] = useState<CarsInfo[]>([])
+  const [carInfo, setCarInfo] = useState<CarInfo>({
+    year: "",
+    fipe: 0,
+    fuel: 0,
+  })
 
   useEffect(() => {
     const makers = Object.keys(repo)
     setMakerList(makers)
   }, [repo])
 
-  const { createPublish, publishInfo, setPublishInfo } = usePublish()
-
-  const handleChange = (e: any) => {
+  const handleChangeModel = async (e: ChangeEvent<HTMLSelectElement>) => {
     const maker = e.target.value
 
     const value = repo[maker]
 
     setModels(value)
+
+    const response = await fetch(
+      `https://kenzie-kars.herokuapp.com/cars?brand=${maker.toLowerCase()}`
+    )
+
+    const responseJson = await response.json()
+
+    setCarsInfo(responseJson)
   }
+
+  const handleChangeCarInfo = (e: ChangeEvent<HTMLSelectElement>) => {
+    const car = carsInfo.filter((car) => car.name == e.target.value)
+
+    setCarInfo({
+      year: car[0].year,
+      fuel: car[0].fuel,
+      fipe: car[0].value,
+    })
+  }
+
   return (
-    <>
+    <form onSubmit={(e) => createPublish(e, carInfo)}>
       <div className="flex flex-col gap-2 mb-4">
         <h1 className="text-base font-medium mb-2">Criar anúncio</h1>
         <p className="text-sm leading-6 font-medium mb-3">
@@ -38,9 +88,10 @@ export const PublishForm = ({ toggleModal, repo }: FormProps) => {
         </p>
         <label className="text-sm font-medium">Marca</label>
         <select
+          id="make"
           className="w-9/10 p-2 border rounded border-grey-grey7"
           onChange={(e) => {
-            handleChange(e)
+            handleChangeModel(e)
             setPublishInfo({ ...publishInfo, make: e.target.value })
           }}
         >
@@ -55,8 +106,10 @@ export const PublishForm = ({ toggleModal, repo }: FormProps) => {
       <div className="flex flex-col gap-2 mb-4">
         <label className="text-sm font-medium">Modelo</label>
         <select
+          id="model"
           className="w-9/10 p-2 border rounded border-grey-grey7"
           onChange={(e) => {
+            handleChangeCarInfo(e)
             setPublishInfo({ ...publishInfo, model: e.target.value })
           }}
         >
@@ -70,32 +123,38 @@ export const PublishForm = ({ toggleModal, repo }: FormProps) => {
 
       <div className="flex gap-4 mb-4">
         <div>
-          <label htmlFor="year" className="user-form-label text-sm font-medium">
+          <label htmlFor="year" className="text-sm font-medium">
             Ano
           </label>
           <div>
             <input
               type="number"
-              onChange={(e) => {
-                setPublishInfo({ ...publishInfo, year: Number(e.target.value) })
-              }}
-              placeholder="2018"
+              id="year"
+              defaultValue={carInfo.year}
+              disabled
+              placeholder="2020"
               className="w-38 p-2 border rounded mt-1 border-grey-grey7"
             />
           </div>
         </div>
 
         <div>
-          <label htmlFor="fuel" className="user-form-label text-sm font-medium">
+          <label htmlFor="fuel" className="text-sm font-medium">
             Combustível
           </label>
           <div>
             <input
+              id="fuel"
+              disabled
+              value={
+                carInfo.fuel == 1
+                  ? "Flex"
+                  : carInfo.fuel == 2
+                  ? "Híbrido"
+                  : "Elétrico"
+              }
               type="text"
-              onChange={(e) => {
-                setPublishInfo({ ...publishInfo, fuel: e.target.value })
-              }}
-              placeholder="Gasolina / Diesel"
+              placeholder="Elétrico"
               className="w-38 p-2 border rounded mt-1 border-grey-grey7"
             />
           </div>
@@ -104,15 +163,14 @@ export const PublishForm = ({ toggleModal, repo }: FormProps) => {
 
       <div className="flex gap-4 mb-4">
         <div>
-          <label
-            htmlFor="distance"
-            className="user-form-label text-sm font-medium"
-          >
+          <label htmlFor="distance" className="text-sm font-medium">
             Kilometragem
           </label>
           <div>
             <input
-              type="number"
+              id="distance"
+              type="text"
+              value={publishInfo.distance}
               onChange={(e) => {
                 setPublishInfo({
                   ...publishInfo,
@@ -126,15 +184,14 @@ export const PublishForm = ({ toggleModal, repo }: FormProps) => {
         </div>
 
         <div>
-          <label
-            htmlFor="color"
-            className="user-form-label text-sm font-medium"
-          >
+          <label htmlFor="color" className="text-sm font-medium">
             Cor
           </label>
           <div>
             <input
+              id="color"
               type="text"
+              value={publishInfo.color}
               onChange={(e) => {
                 setPublishInfo({ ...publishInfo, color: e.target.value })
               }}
@@ -147,13 +204,14 @@ export const PublishForm = ({ toggleModal, repo }: FormProps) => {
 
       <div className="flex gap-4 mb-4">
         <div>
-          <label htmlFor="fipe" className="user-form-label text-sm font-medium">
+          <label htmlFor="fipe" className="text-sm font-medium">
             Preço FIPE
           </label>
           <div>
             <input
-              type="number"
+              type="text"
               disabled
+              value={`R$ ${carInfo.fipe.toFixed(2).replace(".", ",")}`}
               placeholder="48000,00"
               className="w-38 p-2 border rounded mt-1 border-grey-grey7"
             />
@@ -161,37 +219,35 @@ export const PublishForm = ({ toggleModal, repo }: FormProps) => {
         </div>
 
         <div>
-          <label
-            htmlFor="price"
-            className="user-form-label text-sm font-medium"
-          >
+          <label htmlFor="price" className="text-sm font-medium">
             Preço
           </label>
           <div>
             <input
-              type="text"
+              id="price"
+              type="number"
+              value={publishInfo.price}
               onChange={(e) => {
                 setPublishInfo({
                   ...publishInfo,
                   price: Number(e.target.value),
                 })
               }}
-              placeholder="190000,00"
+              placeholder="R$ 190000,00"
               className="w-38 p-2 border rounded mt-1 border-grey-grey7"
             />
           </div>
         </div>
       </div>
       <div className="w-full mb-4">
-        <label
-          htmlFor="description"
-          className="user-form-label text-sm font-medium"
-        >
+        <label htmlFor="description" className="text-sm font-medium">
           Descrição
         </label>
         <div>
           <textarea
+            id="description"
             name="description"
+            value={publishInfo.description}
             onChange={(e) => {
               setPublishInfo({ ...publishInfo, description: e.target.value })
             }}
@@ -200,15 +256,14 @@ export const PublishForm = ({ toggleModal, repo }: FormProps) => {
         </div>
       </div>
       <div>
-        <label
-          htmlFor="coverImg"
-          className="user-form-label text-sm font-medium"
-        >
+        <label htmlFor="coverImg" className="text-sm font-medium">
           Imagem de capa
         </label>
         <div>
           <input
+            id="coverImg"
             type="url"
+            value={publishInfo.coverImg}
             onChange={(e) => {
               setPublishInfo({ ...publishInfo, coverImg: e.target.value })
             }}
@@ -221,15 +276,8 @@ export const PublishForm = ({ toggleModal, repo }: FormProps) => {
 
       <div className="flex justify-end gap-6 my-4">
         <button onClick={toggleModal}>Cancelar</button>
-        <button
-          onClick={(e) => {
-            e.preventDefault()
-            createPublish()
-          }}
-        >
-          Criar anúncio
-        </button>
+        <button type="submit">Criar anúncio</button>
       </div>
-    </>
+    </form>
   )
 }
--- src/contexts/publications.tsx
+++ src/contexts/publications.tsx
@@ -1,3 +1,4 @@
+import { CarInfo } from "@/components/publishForm"
 import { PublishRequest } from "@/schemas/publish.schema"
 import { api } from "@/services/api"
 import {
@@ -14,9 +15,20 @@ interface Props {
 }
 
 interface PublishProviderData {
-  images: string[]
-  setImages: Dispatch<SetStateAction<string[]>>
-  createPublish: () => void
+  images: {
+    image1: string
+    image2: string
+    image3: string
+    image4: string
+    image5: string
+    image6: string
+    image7: string
+    image8: string
+    image9: string
+    image10: string
+  }
+  setImages: Dispatch<SetStateAction<any>>
+  createPublish: (e: { preventDefault: () => void }, carInfo: CarInfo) => void
   publishInfo: PublishRequest
   setPublishInfo: Dispatch<SetStateAction<PublishRequest>>
 }
@@ -26,7 +38,6 @@ const PublishContext = createContext<PublishProviderData>(
 )
 
 export function PublishProvider({ children }: Props) {
-  const [images, setImages] = useState<string[]>([])
   const [publishInfo, setPublishInfo] = useState({
     model: "",
     make: "",
@@ -37,19 +48,46 @@ export function PublishProvider({ children }: Props) {
     price: 0,
     description: "",
     coverImg: "",
+    images: [""],
+  })
+  const [images, setImages] = useState({
+    image1: "",
+    image2: "",
+    image3: "",
+    image4: "",
+    image5: "",
+    image6: "",
+    image7: "",
+    image8: "",
+    image9: "",
+    image10: "",
   })
 
-  const createPublish = () => {
+  const createPublish = async (
+    e: { preventDefault: () => void },
+    carInfo: CarInfo
+  ) => {
+    e.preventDefault()
+    console.log(images)
+    publishInfo.year = Number(carInfo.year)
+    publishInfo.fuel =
+      carInfo.fuel == 1 ? "Flex" : carInfo.fuel == 2 ? "Híbrido" : "Elétrico"
+
+    // try {
+    //   const response = await api.post("/publications", publishInfo)
+    // } catch (error: any) {
+    //   console.log(error.message)
+    // }
     console.log(publishInfo)
   }
   return (
     <PublishContext.Provider
       value={{
+        images,
+        setImages,
         createPublish,
         publishInfo,
         setPublishInfo,
-        setImages,
-        images,
       }}
     >
       {children}
