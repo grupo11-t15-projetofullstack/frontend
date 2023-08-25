96d5ea90d67e3a103ec1b1cace5c17c5f62e5838
Elvys <elvys_campos14@live.com>
2023-08-25 18:18:07 -0300

    add/toast

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,26 @@ import { PublishProvider } from "@/contexts/publications";
 import { UserProvider } from "@/contexts/user";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <UserProvider>
       <PublishProvider>
         <Component {...pageProps} />
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+        />
       </PublishProvider>
     </UserProvider>
   );
